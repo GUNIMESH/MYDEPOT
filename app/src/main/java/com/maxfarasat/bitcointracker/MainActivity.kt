81f65349a5a911115a556f64bdb5eea87ff6cde5
MaxFarasat <maxfarasatali@gmail.com>
2020-01-10 00:30:12 +0530

    Override functions in activity class for menu and menu item.

--- app/src/main/java/com/maxfarasat/bitcointracker/MainActivity.kt
+++ app/src/main/java/com/maxfarasat/bitcointracker/MainActivity.kt
@@ -2,6 +2,8 @@ package com.maxfarasat.bitcointracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.load -> {
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
